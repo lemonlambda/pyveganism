369239ce78f4395721b6972af99c5f4d5c6ff9e5
tiriscef <koun@gmx.de>
2019-11-01 21:56:56 +0100

    removed outdated migration files

--- migrations/pyveganism-register-update.lua
+++
@@ -1,7 +0,0 @@
-if not global.registered_machines then
-    for _, surface in pairs(game.surfaces) do
-        for _, entity in pairs(surface.find_entities_filtered {name = {"pyveganism-beacon-cultivation-expertise", "pyveganism-beacon-plant-breeding"}}) do
-            entity.destroy()
-        end
-    end
-end
--- migrations/pyveganism-tech-unlocks-0.0.2.lua
+++
@@ -1,7 +0,0 @@
--- unlocks all the recipes that I added since last version to existing techs
--- otherwise the player would have to run a console command as the factorio engine doesn't seem to do it by itself
-for _, force in pairs(game.forces) do
-    force.reset_recipes()
-    force.reset_technologies()
-    force.reset_technology_effects()
-end
